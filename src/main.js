5098abc28279dbd06c868308a23f0daa7b10d74c
Frost-ZX <30585462+Frost-ZX@users.noreply.github.com>
2022-03-26 10:17:54 +0800

    安装 Buefy、Material Design Icons、ress

--- src/main.js
+++ src/main.js
@@ -2,7 +2,20 @@ import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 
+import Buefy from 'buefy';
+
+import 'ress/ress.css';
+import 'buefy/dist/buefy.css';
+import '@mdi/font/css/materialdesignicons.css';
+import '@/assets/css/main.less';
+
 Vue.config.productionTip = false;
+
+Vue.use(Buefy, {
+  defaultIconPack: 'mdi',
+  defaultDialogConfirmText: '确定',
+  defaultDialogCancelText: '取消',
+});
 
 new Vue({
   store,
