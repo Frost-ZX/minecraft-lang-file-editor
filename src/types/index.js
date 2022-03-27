2b79c6c35b7d833c1785ee3394665370b353c3ae
Frost-ZX <30585462+Frost-ZX@users.noreply.github.com>
2022-03-28 00:13:13 +0800

    添加 @typedef 注释

---
+++ src/types/index.js
@@ -0,0 +1,26 @@
+/**
+ * @typedef LangItem
+ * @type {Object.<string, string>} 
+ */
+
+/**
+ * @typedef LangItemList
+ * @type {Object.<string, LangItem>}
+ */
+
+/*
+
+  参考格式：
+
+  {
+    'Lang Key': {
+      'en_us': 'Message',
+      'zh_cn': 'Message',
+      ...
+    },
+    ...
+  }
+
+*/
+
+export default {};
