d74ffdc1b82a75559c690f03682ae72d55ed5bd7
Frost-ZX <30585462+Frost-ZX@users.noreply.github.com>
2022-03-28 00:14:20 +0800

    添加 objPagination、toast、uniDecode 和 uniEncode 函数

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -1,39 +1,66 @@
+import { ToastProgrammatic as Toast } from 'buefy';
+
+/**
+ * @typedef ColorType
+ * @type {'is-white'|'is-black'|'is-light'|'is-dark'|'is-primary'|'is-info'|'is-success'|'is-warning'|'is-danger'}
+ */
+
 class Utils {
 
   constructor() { }
 
   /**
-   * @description 改变网页标题
-   * @param {string} [value] 新的标题
+   * @description 分页获取对象数据
+   * @param {object} options
+   * @param {Object.<string, *>} options.obj
+   * @param {number} [options.pageNum]
+   * @param {number} [options.pageSize]
    */
-  changeTitle(value) {
-    document.title = ((value ? `${value} - ` : '') + 'Editor');
-  }
+  objPagination(options) {
 
-  /**
-   * @description JSONP
-   * @param {object} options 配置选项
-   */
-  jsonp(options) {
-    const config = {
-      url: '',
-      cbName: 'cb',
-      cbFunc: function (data) {
-        console.log('[JSONP]', data);
-      },
-    };
-    const el = document.createElement('script');
+    const {
+      obj = null,
+      pageNum = 1,
+      pageSize = 20,
+    } = options;
 
-    Object.assign(config, options);
+    const isValid = (
+      obj && obj.constructor === Object &&
+      typeof pageNum === 'number' &&
+      typeof pageSize === 'number'
+    );
 
-    window[config.cbName] = config.cbFunc;
+    if (!isValid) {
+      console.error('获取数据失败，参数格式错误！');
+      return null;
+    }
 
-    el.addEventListener('load', function () {
-      this.remove();
+    // 起始下标
+    const startIndex = (pageNum - 1) * pageSize;
+    // 结束下标（需要 +1）
+    const endIndex = startIndex + pageSize;
+
+    const allKeys = Object.keys(obj);
+    const useKeys = allKeys.slice(startIndex, endIndex);
+    const maxSize = allKeys.length;
+    const currSize = useKeys.length;
+
+    const newObj = {};
+
+    useKeys.forEach((key) => {
+      newObj[key] = obj[key];
     });
-    el.setAttribute('src', config.url);
 
-    document.body.appendChild(el);
+    return {
+      obj: newObj,
+      maxPage: Math.ceil(maxSize / pageSize),
+      currPage: pageNum,
+      startPos: startIndex + 1,
+      endPos: endIndex,
+      maxSize,
+      currSize,
+    };
+
   }
 
   /**
@@ -92,7 +119,7 @@ class Utils {
       Promise.all(reads).then((res) => {
         // [成功]
         procResolve(res);
-        resetValue && el.setAttribute('value', '');
+        resetValue && (el['value'] = '');
       }).catch(() => {
         // [失败]
         procReject(blank);
@@ -100,6 +127,86 @@ class Utils {
     };
 
     return new Promise(fnProc);
+  }
+
+
+  /**
+   * @description Toast
+   * @param {object} options
+   * @param {number} [options.duration] 时长（毫秒）
+   * @param {boolean} [options.indefinite] 无限时长
+   * @param {string} options.message 消息内容
+   * @param {ColorType} [options.type] 消息类型
+   */
+  toast(options) {
+    const {
+      duration = 2000,
+      indefinite = false,
+      message = '',
+      type = 'is-info',
+    } = options;
+
+    return Toast.open({
+      duration,
+      indefinite,
+      message,
+      pauseOnHover: true,
+      type,
+    });
+  }
+
+  /**
+   * @description Unicode（\u 开头）转字符串
+   * @param {string} codes 需要转换的 Unicode 编码字符串
+   * @returns {string} 转换结果
+   */
+  uniDecode(codes = '') {
+
+    if (typeof codes !== 'string') {
+      console.error('转换失败，参数数据类型错误！');
+      return null;
+    }
+
+    if (codes === '') {
+      return '';
+    }
+
+    const reg = new RegExp('\\\\u', 'g');
+    const converted = codes.replace(reg, '%u');
+
+    try {
+      return unescape(converted);
+    } catch (error) {
+      console.error('转换失败', error);
+      return null;
+    }
+
+  }
+
+  /**
+   * @description 字符串转 Unicode（\u 开头）
+   * @param {string} str 需要转换的字符串
+   * @returns {string} 转换结果
+   */
+  uniEncode(str = '') {
+
+    if (typeof str !== 'string') {
+      console.error('转换失败，参数数据类型错误！');
+      return null;
+    }
+
+    if (str === '') {
+      return '';
+    }
+
+    const codes = [];
+
+    for (let i = 0; i < str.length; i++) {
+      codes.push((str.charCodeAt(i).toString(16).padStart(4, '0')));
+    }
+
+    return ('\\u' + codes.join('\\u'));
+
   }
 
 }
